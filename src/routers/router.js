b6e574e2acb3048810bca1b3ad51f624466cfe9e
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2025-12-11 12:32:54 +0530

    adding sign

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../pages/main";
 import Home from "../pages/home";
+import SignUp from "../pages/signUp";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
         element: <Home />,
       },
     ],
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
   },
 ]);
 export default router;
